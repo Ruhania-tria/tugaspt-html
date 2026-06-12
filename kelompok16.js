fe3aed5d17940c3194139584ae53101f6bd01b73
Ruhania <ruhania486@gmail.com>
2026-06-12 20:48:58 +0800

    buat daftar menu

---
+++ kelompok16.js
@@ -0,0 +1,32 @@
+
+const menu = [
+  {
+    id:1,
+    nama:"Espresso",
+    harga:"Rp 18.000",
+    gambar:"https://images.unsplash.com/photo-1517701604599-bb29b565090c?q=80&w=1200"
+  },
+
+  {
+    id:2,
+    nama:"Cappuccino",
+    harga:"Rp 25.000",
+    gambar:"https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=1200"
+  },
+
+  {
+    id:3,
+    nama:"Latte",
+    harga:"Rp 23.000",
+    gambar:"https://images.unsplash.com/photo-1497636577773-f1231844b336?q=80&w=1200"
+  },
+
+  {
+    id:4,
+    nama:"Americano",
+    harga:"Rp 20.000",
+    gambar:"https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=1200"
+  }
+]
+
+module.exports = menu
